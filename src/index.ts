5fca881b5189102a76512ef7354616fb9d02fe55
expdani <daniverhaar@hotmail.com>
2022-06-20 19:55:07 +0200

    added certificates

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,10 @@ import { Settings } from "./Entities/Settings";
 import { Items } from "./Entities/Items";
 import { RandomEvents } from "./Entities/RandomEvents";
 import { GlobalSettings } from "./Entities/GlobalSettings";
+
 const enforce = require("express-sslify");
+const https = require("node:https");
+const fs = require("fs");
 
 const main = async () => {
   await createConnection({
@@ -57,9 +60,29 @@ const main = async () => {
   // for testing discord api only.
   if (env.NODE_ENV === "development") app.use("/discord", discord.default);
 
-  app.listen(env.PORT, () => {
-    console.log("SERVER RUNNING ON PORT " + env.PORT);
-  });
+  if (env.NODE_ENV === "development")
+    app.listen(env.PORT, () => {
+      console.log("SERVER RUNNING ON PORT " + env.PORT);
+    });
+  else {
+    const privateKey = fs.readFileSync(
+      "/etc/letsencrypt/live/atdani.nl/privkey.pem"
+    );
+    const certificate = fs.readFileSync(
+      "/etc/letsencrypt/live/atdani.nl/cert.pem"
+    );
+
+    console.log(certificate);
+
+    const options = {
+      key: privateKey,
+      cert: certificate,
+    };
+
+    https.createServer(options, app).listen(env.PORT, function () {
+      console.log("HTTPS SERVER RUNNING ON PORT " + env.PORT);
+    });
+  }
 };
 
 main().catch((err) => {
